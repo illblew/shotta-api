aac0b8ee0a171b2c85837e01609cb4697e46a08f
Will Blew <will@willblew.com>
2019-10-31 02:30:59 +0000

    Forgot to say Flask

--- micro.py
+++ micro.py
@@ -2,7 +2,7 @@ import pika
 import uuid
 from flask import Flask, jsonify
 
-app = (__name__)
+app = Flask(__name__)
 
 def send_snap(url):
     data = []
@@ -13,7 +13,7 @@ def send_snap(url):
         connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
         channel = connection.channel()
         channel.queue_declare(queue='shotta_snap')
-        channel.basic_publish(exchange='',routing_key='shoota_snap', body=data_body)
+        channel.basic_publish(exchange='',routing_key='shotta_snap', body=data_body)
         return { "shot_id" : uid }
     except Exception as e:
         return { "error" : "Shotta appears to have encountered an issues. Try again." }
